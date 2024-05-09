2d010a3adfbed07438d997774ae259d7635d8e22
James Mackman <jmackman@live.co.uk>
2024-05-09 13:34:58 +0000

    Connect external database to code

--- storyspot/settings.py
+++ storyspot/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d%&3+*h8m7zgp&7kylncp#myfj&f50hdtf=0l$o2rkqdxb$j@#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-jamesmackman-storyspot-fodsf8zmeaj.ws-eu111.gitpod.io', ".herokuapp.com"]
 
