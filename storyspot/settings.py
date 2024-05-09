e18e2902e03aa892945c84795ac044946ca3a9d9
James Mackman <jmackman@live.co.uk>
2024-05-09 13:51:29 +0000

    Add secret key

--- storyspot/settings.py
+++ storyspot/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d%&3+*h8m7zgp&7kylncp#myfj&f50hdtf=0l$o2rkqdxb$j@#'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
